70795286c8b1dea9ada1ad1e12686d0e45574e95
HuangSenhai <huangsenhai621@gmail.com>
2021-12-30 16:11:57 +0800

    老子都提交了

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer">
+    <span class="todo-count">剩余 <strong>{{ farr.length }}</strong></span>
+    <ul class="filters" @click="fn">
+      <li>
+        <a :class="{selected: isSel === 'all'}" href="javascript:;" @click="isSel='all'">全部</a>
+      </li>
+      <li>
+        <a :class="{selected: isSel === 'no'}" href="javascript:;" @click="isSel='no'">未完成</a>
+      </li>
+      <li>
+        <a :class="{selected: isSel === 'yes'}" href="javascript:;" @click="isSel='yes'">已完成</a>
+      </li>
+    </ul>
+    <button class="clear-completed" @click="clearFn">清除已完成</button>
+  </footer>
+</template>
+
+<script>
+export default {
+    props: ['farr'],
+    data() {
+      return {
+        isSel: 'all'
+      }
+    },
+    methods: {
+      fn() {
+        this.$emit("changeType",this.isSel)
+      },
+      clearFn() {
+        this.$emit("clear")
+      }
+    }
+}
+</script>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="todo-list">
+    <!-- completed: 完成的类名 -->
+    <li :class="{completed:obj.isDone}" v-for="obj in arr" :key="obj.id" >
+      <div class="view">
+        <input class="toggle" type="checkbox" v-model="obj.isDone"/>
+        <label>{{ obj.name }}</label>
+        <button @click="delFn(obj.id)" class="destroy"></button>
+      </div>
+    </li>
+  </ul>
+  
+</template>
+
+<script>
+export default {
+    props:["arr"],
+    methods: {
+        delFn(id) {
+            this.$emit('del',id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
